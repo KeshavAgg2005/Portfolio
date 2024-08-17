b8cedd00633246c7903ca93a0a00de44ec01f539
KeshavAgg2005 <keshava@iitbhilai.ac.in>
2024-08-17 16:30:22 +0530

    Updated

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ $(document).ready(function() {
     });
 
   //contact form to excel sheet
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbzUSaaX3XmlE5m9YLOHOBrRuCh2Ohv49N9bs4bew7xPd1qlgpvXtnudDs5Xhp3jF-Fx/exec';
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwBfV6rX8BUTEpYm8KD08yV_bx35zwWxAcJlNQSj99qalF9tXBgy2KVxQwYjl9JqNFUjA/exec';
   const form = document.forms['submitToGoogleSheet']
   const msg = document.getElementById("msg")
 
